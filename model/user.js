29bb5382fcfbd2b7991bcfb9d4c97d716eea0392
sandix34 <sandrinezocca@gmail.com>
2022-02-23 16:00:44 +0100

    create user model

---
+++ model/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    password: {
+        type: String,
+        required: [true,"Password is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email address is required"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+console.log(User);
+
+module.exports = User;
