a6022611d5648008301701363b831a8b134547c9
sandix34 <sandrinezocca@gmail.com>
2022-02-24 14:54:11 +0100

    create user in database

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,28 @@
+const User = require("../model/user");
 
 exports.registrationForm = (req, res, next) => {
     res.render('signup');
 }
 
 exports.userCreate = async (req, res, next) => {
-    res.end();
+    try {
+        const newUser = new User({
+            email: req.body.email,
+            name: req.body.name,
+            password: req.body.password
+        });
+        await newUser.save(err => {
+            if (err) {
+                return res.status(400).json("registration failed");
+            } else {
+                res.json({
+                    data: newUser
+                });
+            }
+        })
+        console.log(newUser);
+    } catch (error) {
+        next(error);
+    }
 };
 
